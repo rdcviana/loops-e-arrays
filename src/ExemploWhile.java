0f941a308b4e688f820dca1f0e7a7aedd0d1ca38
Rodrigo Viana <rodrigodcviana@gmail.com>
2023-03-17 17:31:22 -0300

    Melhoria na implementação do programa ExemploWhile.java

--- src/ExemploWhile.java
+++ src/ExemploWhile.java
@@ -17,11 +17,12 @@ public class ExemploWhile {
         String nome;
         int idade;
 
-        //Aqui solicitaremos o nome do aluno e em seguida fazer a verificação no while
-        System.out.print("Digite o nome do aluno: ");
-        nome = sc.nextLine();
-
-        while (!nome.equals("0")) {
+        while (true) {
+            //Aqui solicitaremos o nome do aluno e em seguida fazer a verificação no while
+            System.out.print("Digite o nome do aluno: ");
+            nome = sc.nextLine();
+            if (nome.equals("0"))
+                break;
             System.out.print("Digite a idade do(a) aluno(a) " + nome + ": ");
             idade = sc.nextInt();
             System.out.println();
@@ -31,15 +32,10 @@ public class ExemploWhile {
             System.out.println("Idade do(a) aluno(a): " + idade);
             System.out.println();
 
-            //Aqui a quebra de linha anterior é consumida
             sc.nextLine();
-
-            //Solicitação do nome de um novo aluno e uma nova verificação no while será realizada.
-            System.out.print("Digite o nome do aluno: ");
-            nome = sc.nextLine();
         }
-
         //Mensagem informando a finalização do programa
+        System.out.println();
         System.out.println("Programa finalizado!");
 
         sc.close();
